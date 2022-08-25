94d3d3cf8e0ad47ac5a011256d24e776e25ec1c2
KevinYSM <22753615@student.uwa.edu.au>
2022-08-25 13:26:43 +0800

    Working navigation bar and page routing

--- refood-pwa/src/App.js
+++ refood-pwa/src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import logo from './assets/logo.png';
+//import logo from './assets/images/logo.png';
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Explore from "./pages/Explore"
+import Inbox from "./pages/Inbox"
+import Orders from "./pages/Orders"
+import Profile from "./pages/Profile"
 
 function App() {
+  let component
+  switch(window.location.pathname){
+   
+    case "/":
+      component = <App/>
+      break
+    case "/explore":
+      component = <Explore />
+      break
+    case "/inbox":
+      component = <Inbox />
+      break
+    case "/orders":
+      component = <Orders />
+      break
+    case "/profile":
+      component = <Profile />
+      break
+  }
   return (
-    <h1> hello</h1>
+    <>
+    {component}
+    <Navbar />
+  
+    
+    </>
+    
+      
+    
   );
 }
 
---
+++ refood-pwa/src/pages/Orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Orders(props) {
+    return (
+        <div>
+            Orders page
+        </div>
+    );
+}
+
+export default Orders;
