620c4a2f7ef849584c8a758504b2646d52a3ae54
bayleyr <103823994+bayleyr@users.noreply.github.com>
2022-08-29 16:06:06 +0800

    fixed default route

--- refood-pwa/src/App.js
+++ refood-pwa/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
 import './App.css';
+//import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Explore from "./pages/Explore"
 import Inbox from "./pages/Inbox"
 import Orders from "./pages/Orders"
 import Profile from "./pages/Profile"
+import Home from "./pages/Home"
 
 function App() {
   let component
   switch(window.location.pathname){
     case "/":
-      component = <App/>
+      component = <Home/>
       break
     case "/explore":
       component = <Explore />
@@ -25,23 +27,12 @@ function App() {
       component = <Profile />
       break
     default:
-      component = <App/>
+      component = <Home />
       break
   }
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <>
       {component}
       <Navbar />
