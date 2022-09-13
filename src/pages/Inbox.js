2e58eb3e60c5827de9d57251a140cc8fb336cd42
sumit951012 <84025039+sumit951012@users.noreply.github.com>
2022-09-13 21:17:34 +0800

    inbox

--- src/pages/Inbox.js
+++ src/pages/Inbox.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import styles from "./Inbox.css";
+
+function TabItem(props) {
+    return <div {...props} />;
+  }
+  
+function Tabs(props) {
+    const [bindIndex, setBindIndex] = React.useState(props.defaultIndex);
+    const changeTab = newIndex => {
+      if (typeof props.onTabClick === "function") props.onTabClick(newIndex);
+      setBindIndex(newIndex);
+    };
+    const items = props.children.filter(item => item.type.name === TabItem.name);
+  
+    return (
+      <div className={styles["wrapper"]}>
+        <div className={styles["tab-menu"]}>
+          {items.map(({ props: { index, label } }) => (
+            <button
+              onClick={() => changeTab(index)}
+              key={index}
+              className={bindIndex === index ? styles["focus"] : ""}
+            >
+              {label}
+            </button>
+          ))}
+        </div>
+        <div className={styles["tab-view"]}>
+          {items.map(({ props }) => (
+            <div
+              {...props}
+              className={styles["tab-view_item"]}
+              key={props.index}
+              style={{ display: bindIndex === props.index ? "block" : "none" }}
+            />
+          ))}
+        </div>
+      </div>
+    );
+}
 
 function Inbox(props) {
     return (
-        <div>
-            Inbox page
-        </div>
+      <div>
+        <Tabs defaultIndex="1" onTabClick={console.log}>
+            <TabItem label="Messages" index="1">
+                <h5>You have no unread Messages</h5>
+                <p>When you contact the business to collect food or send a reservation request, you'll find you messages here.</p>
+            </TabItem>
+            <TabItem label="Notifications" index="2">
+                <h5>You have no unread Notifications</h5>
+                <p>When you contact the business to collect food or send a reservation request, you'll find you Notifications here.</p>
+            </TabItem>
+        </Tabs>
+      </div>
     );
 }
 
