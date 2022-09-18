33accac8bc2ad8865bceedd6015d082e5ecb5812
Bsubs <bsubs02@gmail.com>
2022-09-18 13:39:16 +0800

    Profile page now correctly renders user details

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,16 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Amplify, Auth } from 'aws-amplify';
-
-async function getUser() {
-    let user = await Auth.currentAuthenticatedUser();
-
-    const { attributes } = user;
-    console.log("Test");
-    console.log(user);
-}
+import { type } from '@testing-library/user-event/dist/type';
 
 function Explore(props) {
-    getUser();
+
     return (
         <div>
             Explore Page
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Amplify, Auth } from 'aws-amplify';
+import { type } from '@testing-library/user-event/dist/type';
 import './Profile.css';
 import profile from './profile-circle.png'
 import lock from './lock.png'
@@ -7,6 +9,24 @@ import chevrons from './chevrons-right.png'
 import book from './book.png'
 import banner from "./bakers-delight.jpg"
 function Profile(props) {
+
+    const [attributes, setAttributes] = useState({});
+
+    useEffect(() => {
+        fetchAttributes();
+      }, []);
+
+    const fetchAttributes = async() => {
+        try{
+            const userData = await Auth.currentAuthenticatedUser();
+            const attributesList = userData.attributes;
+            setAttributes(attributesList);
+        } catch (error) {
+            console.log('error in fetching user data', error);
+        }
+
+    };
+
     return (
         <div id="profile_page">
       
@@ -87,6 +107,30 @@ function Profile(props) {
                                 <img className="arrow" src={chevrons} alt="select icon"/>
                             </div>
                         </a> 
+                    </li>
+                </ul>
+                <h1>User Details</h1>
+                <ul>
+                    <li>
+                        <div className='list_member'>{attributes.given_name} {attributes.family_name}</div>
+                    </li>
+                    <li>
+                        <div className='list_member'>{attributes.email}</div>
+                    </li>
+                    <li>
+                        <div className='list_member'>{attributes.phone_number}</div>
+                    </li>
+                    <li>
+                        <div className='list_member'>{attributes['custom:business_name']}</div>
+                    </li>
+                    <li>
+                        <div className='list_member'>{attributes['address']}</div>
+                    </li>
+                    <li>
+                        <div className='list_member'>{attributes['custom:abn']}</div>
+                    </li>
+                    <li>
+                        <div className='list_member'>{attributes['custom:type']}</div>
                     </li>
                 </ul>
             </div>
