7da2be896e7d2b2e3e33cfdf57c4114a695b7667
KevinYSM <22753615@student.uwa.edu.au>
2022-10-05 19:55:35 +0800

    Finished notifications page

--- src/pages/Profile/Notifications/Notifications.js
+++ src/pages/Profile/Notifications/Notifications.js
@@ -3,6 +3,58 @@ import './Notifications.css';
 
 export default function Notifications() {
   return (
-    <div id="notifications_page">Notifications</div>
+    <div id="notifications_page">
+        <div className="header">Notifications</div>
+        <div id="nearby_donations_notification" className="notification_option">
+            <label className="switch">
+                <input type="checkbox"></input>
+                <span className="slider round"></span>
+            </label>
+            <div className="donation_text"> 
+             <div className="donation_text_header">Nearby Donations</div>
+             <div className="donation_text_description">Notify me when donations are nearby</div>
+            </div>
+        </div>
+
+        <div id="message_notification" className="notification_option">
+            <label className="switch">
+                <input type="checkbox"></input>
+                <span className="slider round"></span>
+            </label>
+            <div className="donation_text"> 
+             <div className="donation_text_header">Message</div>
+             <div className="donation_text_description">Notify me when I receive a message</div>
+            </div>
+        </div>
+
+        <div className="header">Privacy</div>
+        <div id="share_phone_details" className="notification_option">
+            <label className="switch">
+                <input type="checkbox"></input>
+                <span className="slider round"></span>
+            </label>
+            <div className="donation_text"> 
+             <div className="donation_text_header">Share Phone Number</div>
+             <div className="donation_text_description">Share phone number with matched donations</div>
+            </div>
+        </div>
+
+        <div id="contacted_by_developers" className="notification_option">
+            <label className="switch">
+                <input type="checkbox"></input>
+                <span className="slider round"></span>
+            </label>
+            <div className="donation_text"> 
+             <div className="donation_text_header">Give Feedback</div>
+             <div className="donation_text_description">App developers can message you and ask for feedback</div>
+            </div>
+        </div>
+
+
+        <button  className="button">
+           
+          Save
+        </button>
+    </div>
   )
 }
