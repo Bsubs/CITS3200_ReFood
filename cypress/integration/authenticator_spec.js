e33ebd2f2514b71c27e7bab7edd09e4eb34f3679
Bsubs <bsubs02@gmail.com>
2022-10-10 17:04:00 +0800

    Added more sign in tests

--- cypress/integration/authenticator_spec.js
+++ cypress/integration/authenticator_spec.js
@@ -2,16 +2,41 @@ describe('Authenticator:', function() {
   beforeEach(function() {
       cy.visit('/');
   });
-  describe('Sign In:', () => {
-      it('allows a user to signin and signout', () => {
-          cy.get('[id=accept_terms_button]').click();
-          cy.get('button').contains('Sign In').click();
-          cy.get('[placeholder="Email"]').type('refood3200@gmail.com');
-          cy.get('[placeholder="Password"]').type('Cits3200@');
-          cy.get('button').contains('Sign in').click();
-          cy.wait(5000);
-          cy.get('a[href*="inbox"]').click();
-          cy.get('button').contains('Sign out').click();
-      });
+
+  describe('No email', () => {
+    it('tests that application responds to wrong password', () => {
+        cy.get('[id=accept_terms_button]').click();
+        cy.get('button').contains('Sign In').click();
+        cy.get('[placeholder="Password"]').type('Cits3200');
+        cy.get('button').contains('Sign in').click();
+        cy.wait(5000);
+        cy.get().contains('Please');
+    });
   });
+
+  // describe('Wrong Password', () => {
+  //   it('tests that application responds to wrong password', () => {
+  //       cy.get('[id=accept_terms_button]').click();
+  //       cy.get('button').contains('Sign In').click();
+  //       cy.get('[placeholder="Email"]').type('refood3200@gmail.com');
+  //       cy.get('[placeholder="Password"]').type('Cits3200');
+  //       cy.get('button').contains('Sign in').click();
+  //       cy.wait(5000);
+  //       cy.get('div').should('have.class', 'amplify-alert__body').contains('Incorrect username or password');
+  //   });
+  // });
+
+  // describe('Sign In:', () => {
+  //     it('allows a user to signin and signout', () => {
+  //         cy.get('[id=accept_terms_button]').click();
+  //         cy.get('button').contains('Sign In').click();
+  //         cy.get('[placeholder="Email"]').type('refood3200@gmail.com');
+  //         cy.get('[placeholder="Password"]').type('Cits3200@');
+  //         cy.get('button').contains('Sign in').click();
+  //         cy.wait(5000);
+  //         cy.url().should('include', '/explore')
+  //         cy.get('a[href*="inbox"]').click();
+  //         cy.get('button').contains('Sign out').click();
+  //     });
+  // });
 });
