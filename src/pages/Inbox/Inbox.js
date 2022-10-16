c66c0c1e370ac63d7f9635ce4264915864ff6b22
sumit951012 <84025039+sumit951012@users.noreply.github.com>
2022-10-16 11:35:20 +0800

    chat

--- src/pages/Inbox/Inbox.js
+++ src/pages/Inbox/Inbox.js
@@ -1,78 +1,118 @@
-import React from 'react';
-import styles from "./Inbox.css";
-import contents from './content.js';
+import React, { useState, useEffect } from 'react';
+import "./Inbox.css";
+import Dialog from './Dialog';
+import { arrayToDict, dialogDataV2 } from './content';
 
-function TabItem(props) {
-    return <div {...props} />;
+import InboxItem from '../../components/inbox/InboxItem';
+import GroupTitle from '../../components/inbox/GroupTitle';
+import InboxTitleBar from '../../components/inbox/Titlebar';
+import { shouldInsertTimestamp } from './util';
+
+const NO_DIALOG_ID = -1;
+
+export default function Inbox() {
+
+  const [dialogData, setDialogData] = useState({});
+
+  const [dialogId, setDialogId] = useState(NO_DIALOG_ID);
+
+  useEffect(() => {
+    setDialogData(arrayToDict(dialogDataV2.map(item => {
+      const [hour, min] = item.timestamp.split(':');
+      return {
+        ...item,
+        timestampNum: Number(hour) * 60 + Number(min)
+      }
+    })))
+  }, [])
+
+  const onSendMsg = (msg) => {
+    if (dialogId === NO_DIALOG_ID) return;
+    const time = new Date();
+    const min = time.getMinutes();
+    const hour = time.getHours();
+    const timestamp = `${hour < 10 ? '0' + hour : hour}:${min < 10 ? '0' + min : min}`
+
+    const current = { ...dialogData[dialogId] };
+    current.content.push({
+      type: 'sent',
+      content: msg,
+      timestamp: shouldInsertTimestamp(current) ? timestamp : undefined,
+    })
+
+    let newDict = { ...dialogData };
+    newDict[dialogId] = current;
+    setDialogData(newDict);
   }
-  
-function Tabs(props) {
-    const [bindIndex, setBindIndex] = React.useState(props.defaultIndex);
-    const changeTab = newIndex => {
-      if (typeof props.onTabClick === "function") props.onTabClick(newIndex);
-      setBindIndex(newIndex);
-    };
-    const items = props.children.filter(item => item.type.name === TabItem.name);
-  
+
+  const onBack = () => {
+    setDialogId(NO_DIALOG_ID);
+  }
+
+  const onClickItem = (e, id) => {
+    e.preventDefault();
+    // zero unread
+    const current = dialogData[id];
+    current.unread = 0;
+    let newDict = { ...dialogData };
+    newDict[id] = current;
+    setDialogData(newDict);
+    setDialogId(id);
+  }
+
+  if (dialogId === NO_DIALOG_ID) {
+
+    const sorter = (a, b) => {
+      const x = dialogData[a];
+      const y = dialogData[b];
+      if (x.unread === 0 && y.unread === 0 || x.unread !== 0 && y.unread !== 0) {
+        return y.timestampNum - x.timestampNum;
+      }
+      if (x.unread !== 0) {
+        return -1;
+      }
+      return 1;
+    }
+
+    const systemNotificationIds = Object.keys(dialogData).filter((key) => {
+      return dialogData[key].userId.startsWith('system:')
+    }).sort(sorter)
+
+    const userMsgIds = Object.keys(dialogData).filter((key) => {
+      return dialogData[key].userId.startsWith('user:')
+    }).sort(sorter)
+
+
     return (
-      <div className={styles["wrapper"]}>
-        <div className={"tab-menu"}>
-          {items.map(({ props: { index, label } }) => (
-            <button
-              onClick={() => changeTab(index)}
-              key={index}
-              className={bindIndex === index ? styles["focus"] : "buttonTab"}
-            >
-              {label}
-            </button>
-          ))}
-        </div>
-        <div className={styles["tab-view"]}>
-          {items.map(({ props }) => (
-            <div
-              {...props}
-              className={styles["tab-view_item"]}
-              key={props.index}
-              style={{ display: bindIndex === props.index ? "block" : "none" }}
-            />
-          ))}
+      <div className='inbox-wrapper'>
+        <InboxTitleBar title={'Inbox'} />
+        <div className='list-wrapper'>
+          <div>
+            <GroupTitle text={'Notifications'} />
+            {systemNotificationIds.map(id => {
+              return (
+                <InboxItem key={id} data={dialogData[id]} onClick={(e) => onClickItem(e, id)} />
+              )
+            })}
+          </div>
+          <div>
+            <GroupTitle text={'Messages'} />
+            {userMsgIds.map(id => {
+              return (
+                <InboxItem key={id} data={dialogData[id]} onClick={(e) => onClickItem(e, id)} />
+              )
+            })}
+
+          </div>
+
         </div>
       </div>
-    );
-}
-
-function Inbox(props) {
-    var Unread_Messages=contents.filter(content => content.isRead!="True");
-    var Read_Messages=contents.filter(content => content.isRead=="True");
-
+    )
+  }
+  else {
     return (
-      <div>
-        <h1>Inbox</h1>
-      <div>
-        <Tabs defaultIndex="1" onTabClick={console.log}>
-            <TabItem label="Messages" index="1">
-                <div id="Unread_Messages_header" className="Unread_type">Unread Messages</div>
-                <div id="No_Unread_Message">
-                  <p class="thick">You have no unread Messages</p>
-                  <p>When you contact the business to collect food or send a reservation request, you'll find you messages here.</p>
-                </div>
-                
-                <div id="Read_Messages_header" className="read_type">Read Messages</div>
-            
-            </TabItem>
-            <TabItem label="Notifications" index="2">
-                <div id="Unread_Notifications_header" className="Unread_type">Unread Notifications</div>
-                
-                <p class="thick">You have no unread Notifications</p>
-                <p>When you contact the business to collect food or send a reservation request, you'll find you Notifications here.</p>
-                
-                <div id="Read_Notifications_header" className="read_type">Read Notifications</div>
-            
-            </TabItem>
-        </Tabs>
-      </div>
-      </div>
-    );
+      <Dialog id={dialogId} data={dialogData[dialogId]}
+        onSendMsg={onSendMsg} onBack={onBack}/>
+    )
+  }
 }
-
-export default Inbox;
