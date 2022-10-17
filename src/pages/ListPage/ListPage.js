883fa6a14d2b43ebc8b53f8c2f66c36834499764
KevinYSM <22753615@student.uwa.edu.au>
2022-10-17 15:05:01 +0800

    bug fixes (oops)

--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -23,10 +23,6 @@ import Modal from '@mui/material/Modal';
 
 import IndividualProduct from "../IndividualProduct/IndividualProduct";
 
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
 
 function ListPage(props) {
   
@@ -309,21 +305,6 @@ useEffect(() => {
      fetchDonations();
    }, []);
 
-   const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-    };
   return (
     <>
   
